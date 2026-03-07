ca87765704e78e9d27d1d462c8628d990d9d4f50
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-03-07 19:34:07 +0900

    feat: 회원 단건조회 API 구현 및 테스트  (#2)
    * feat: 회원 단건 조회 API 구현
    
    * test: 조회 API 테스트

---
+++ src/main/java/app/backend/api/member/dto/MemberResponse.java
@@ -0,0 +1,4 @@
+package app.backend.api.member.dto;
+
+public record MemberResponse(Long id, String name) {
+}
--- src/test/java/app/backend/BackendApplicationTests.java
+++ src/test/java/app/backend/BackendApplicationTests.java
@@ -1,13 +1,13 @@
 package app.backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
 class BackendApplicationTests {
 
     @Test
-    void contextLoads() {
+    void sanity() {
+        assertTrue(true);
     }
 
 }
